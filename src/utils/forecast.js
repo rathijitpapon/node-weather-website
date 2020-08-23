05a293de5c1175636918381019bd31f5cdbea3dc
Rathijit Paul <alienpapon009@gmail.com>
2020-08-23 19:50:36 +0600

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=d486dbe9cacdd7cfd08109bf2ed8f01b'
+
+    request({
+        url,
+        json: true
+    }, (error, {
+        body
+    }) => {
+        if (error) {
+            callback("Unable to connect weather service!", undefined);
+        } else if (body.cod != 200) {
+            callback("Unable to find location", undefined);
+        } else {
+            callback(undefined, "It's currently " +
+                (body.main.temp - 273.15) +
+                " degree out. Current weather is " +
+                body.weather[0].description)
+        }
+    })
+}
+
+
+module.exports = forecast
